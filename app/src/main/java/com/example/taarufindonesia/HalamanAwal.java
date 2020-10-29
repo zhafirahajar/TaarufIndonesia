18ba56d3419389c331833ed61a023f8e7cdd4766
Rizki <rizkiagung01>
2020-10-29 12:53:54 +0700

    Punya rizki

---
+++ app/src/main/java/com/example/taarufindonesia/HalamanAwal.java
@@ -0,0 +1,35 @@
+package com.example.taarufindonesia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HalamanAwal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman_awal);
+        final Button submitButton = (Button)findViewById(R.id.Login);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(HalamanAwal.this, HalamanLogin.class);
+                startActivity(pindah);
+                finish();
+            }
+        });
+        final Button submitButton2 = (Button)findViewById(R.id.Register);
+        submitButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(HalamanAwal.this, HalamanRegis.class);
+                startActivity(pindah);
+                finish();
+            }
+        });
+    }
+}
