947c1c007ad0d895e883facf3169dd7f9261af2e
Rizki <rizkiagung01>
2020-11-01 00:49:38 +0700

    Passing Data Manual

--- app/src/main/java/com/example/taarufindonesia/HalamanAwal.java
+++ app/src/main/java/com/example/taarufindonesia/HalamanAwal.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class HalamanAwal extends AppCompatActivity {
 
@@ -13,16 +14,26 @@ public class HalamanAwal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_awal);
-        final Button submitButton = (Button)findViewById(R.id.Login);
+        //-------------- kode passing data --------------
+        final Button submitButton = (Button) findViewById(R.id.Login);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent pindah = new Intent(HalamanAwal.this, HalamanLogin.class);
-                startActivity(pindah);
-                finish();
+            public void onClick(View v) {
+                Bundle extras = getIntent().getExtras();
+                String inputfullName = extras.getString("fullNameString");
+                String inputAlamat = extras.getString("AlamatString");
+                String inputPekerjaan = extras.getString("PekerjaanString");
+                String inputTglLahir = extras.getString("TglLahirString");
+                Intent submitIntent = new Intent(HalamanAwal.this, HalamanLogin.class);
+                submitIntent.putExtra("fullNameString", inputfullName);
+                submitIntent.putExtra("AlamatString", inputAlamat);
+                submitIntent.putExtra("PekerjaanString", inputPekerjaan);
+                submitIntent.putExtra("TglLahirString", inputTglLahir);
+                startActivity(submitIntent);
             }
         });
-        final Button submitButton2 = (Button)findViewById(R.id.next_kriteria);
+        //-------------- kode passing data --------------
+        final Button submitButton2 = (Button)findViewById(R.id.Register);
         submitButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/taarufindonesia/profile_pribadi.java
+++ app/src/main/java/com/example/taarufindonesia/profile_pribadi.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class profile_pribadi extends AppCompatActivity {
 
@@ -12,7 +13,23 @@ public class profile_pribadi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_pribadi);
+        //-------------- kode passing data --------------
+        Bundle extras = getIntent().getExtras();
+        String inputfullName = extras.getString("fullNameString");
+        String inputAlamat = extras.getString("AlamatString");
+        String inputPekerjaan = extras.getString("PekerjaanString");
+        String inputTglLahir = extras.getString("TglLahirString");
+        TextView showfullName = (TextView) findViewById(R.id.Namalengkap);
+        TextView showAlamat = (TextView) findViewById(R.id.alamat_value);
+        TextView showPekerjaan = (TextView) findViewById(R.id.pekerjaanPribadi_value);
+        TextView showTglLahir = (TextView) findViewById(R.id.tanggalLahir_value);
+        showfullName.setText(inputfullName);
+        showAlamat.setText(inputAlamat);
+        showPekerjaan.setText(inputPekerjaan);
+        showTglLahir.setText(inputTglLahir);
+        //-------------- kode passing data --------------
     }
+
     public void backButtonPressed(View view){
         this.onBackPressed();
     } //di app
