6fc9a409f25f10db74b6beccac5f13b9132d7a2a
Rizki <rizkiagung01>
2020-12-18 18:39:28 +0700

    SwipeCard

--- app/src/main/java/com/example/taarufindonesia/main_menu.java
+++ app/src/main/java/com/example/taarufindonesia/main_menu.java
@@ -4,46 +4,102 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+
+import java.util.ArrayList;
 
 public class main_menu extends AppCompatActivity {
+
+    private ArrayList<String> al;
+    private ArrayAdapter<String> arrayAdapter;
+    private int i;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        //-------------- kode passing data --------------
-        final Button submitButton2 = (Button) findViewById(R.id.button_profile);
-        submitButton2.setOnClickListener(new View.OnClickListener() {
+
+
+        al = new ArrayList<>();
+        al.add("user1");
+        al.add("user2");
+        al.add("user3");
+        al.add("user4");
+        al.add("user5");
+        al.add("user6");
+        al.add("user7");
+        al.add("user8");
+
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.helloText, al );
+
+        SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
+
+        flingContainer.setAdapter(arrayAdapter);
+        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
             @Override
-            public void onClick(View v) {
-                Bundle extras = getIntent().getExtras();
-                String inputfullName = extras.getString("fullNameString");
-                String inputAlamat = extras.getString("AlamatString");
-                String inputPekerjaan = extras.getString("PekerjaanString");
-                String inputTglLahir = extras.getString("TglLahirString");
-                Intent submitIntent = new Intent(main_menu.this, profile_pribadi.class);
-                submitIntent.putExtra("fullNameString", inputfullName);
-                submitIntent.putExtra("AlamatString", inputAlamat);
-                submitIntent.putExtra("PekerjaanString", inputPekerjaan);
-                submitIntent.putExtra("TglLahirString", inputTglLahir);
-                startActivity(submitIntent);
+            public void removeFirstObjectInAdapter() {
+                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+                Log.d("LIST", "removed object!");
+                al.remove(0);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onLeftCardExit(Object dataObject) {
+                //Do something on the left!
+                //You also have access to the original object.
+                //If you want to use it just cast it (String) dataObject
+                Toast.makeText(main_menu.this, "Left", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRightCardExit(Object dataObject) {
+                Toast.makeText(main_menu.this, "Right", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int itemsInAdapter) {
+                // Ask for more data here
+                al.add("XML ".concat(String.valueOf(i)));
+                arrayAdapter.notifyDataSetChanged();
+                Log.d("LIST", "notified");
+                i++;
+            }
+
+            @Override
+            public void onScroll(float scrollProgressPercent) {
+
             }
         });
-        //-------------- kode passing data --------------
-    }
-    public void listMatch(View view){
-        Intent next = new Intent(this, ListMatches.class);
-        startActivity(next);
-    }
 
+
+        // Optionally add an OnItemClickListener
+        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int itemPosition, Object dataObject) {
+                Toast.makeText(main_menu.this, "Click", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
     public void editProfile(View view){
         Intent next = new Intent(this, profile_pribadi.class);
         startActivity(next);
     }
     public void BackHome(View view){
         Intent next = new Intent(this, HalamanAwal.class);
+        startActivity(next);
+    }
+    public void listMatch(View view){
+        Intent next = new Intent(this, ListMatches.class);
         startActivity(next);
     }
 }
