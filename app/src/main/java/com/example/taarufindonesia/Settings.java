36f604be519754710b91cbbddb750a7c393ae2c9
Zhafira Hajar <zfrhjr@gmail.com>
2021-01-10 23:05:19 +0700

    FINAL AKUN

--- app/src/main/java/com/example/taarufindonesia/Settings.java
+++ app/src/main/java/com/example/taarufindonesia/Settings.java
@@ -1,5 +1,6 @@
 package com.example.taarufindonesia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,36 +10,101 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Settings extends AppCompatActivity {
+    private EditText fullName, tanggalLahir, alamat, pekerjaanPribadi;
+    private Button simpan;
+    private FirebaseAuth mAuth;
+    private DatabaseReference userDatabse;
+    private String userId, name, address, occupation, dob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        //-------------- kode passing data --------------
-        Bundle extras = getIntent().getExtras();
-        String inputfullName = extras.getString("fullNameString");
-        String inputAlamat = extras.getString("AlamatString");
-        String inputPekerjaan = extras.getString("PekerjaanString");
-        String inputTglLahir = extras.getString("TglLahirString");
-        TextView showfullName = (EditText) findViewById(R.id.fullName_value);
-        TextView showAlamat = (EditText) findViewById(R.id.alamat_value);
-        TextView showPekerjaan = (EditText) findViewById(R.id.pekerjaanPribadi_value);
-        TextView showTglLahir = (EditText) findViewById(R.id.tanggalLahir_value);
-        showfullName.setText(inputfullName);
-        showAlamat.setText(inputAlamat);
-        showPekerjaan.setText(inputPekerjaan);
-        showTglLahir.setText(inputTglLahir);
-        //-------------- kode passing data --------------
 
+        fullName = (EditText) findViewById(R.id.fullName_value);
+        tanggalLahir = (EditText) findViewById(R.id.tanggalLahir_value);
+        alamat = (EditText) findViewById(R.id.alamat_value);
+        pekerjaanPribadi = (EditText) findViewById(R.id.pekerjaanPribadi_value);
+        simpan = (Button) findViewById(R.id.simpan);
+
+        mAuth = FirebaseAuth.getInstance();
+        userId = mAuth.getCurrentUser().getUid();
+        userDatabse = FirebaseDatabase.getInstance().getReference().child("user").child(userId);
+
+        getUserInfo();
+
+        simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveUserInformation();
+                Intent next = new Intent(Settings.this, profile_pribadi.class);
+                startActivity(next);
+                return;
+            }
+        });
+
+    }
+
+    private void saveUserInformation() {
+        name = fullName.getText().toString();
+        address = alamat.getText().toString();
+        occupation = pekerjaanPribadi.getText().toString();
+        dob = tanggalLahir.getText().toString();
+
+        Map userInfo = new HashMap();
+        userInfo.put("name", name);
+        userInfo.put("address",address);
+        userInfo.put("dob",dob);
+        userInfo.put("occupation",occupation);
+
+        userDatabse.updateChildren(userInfo);
     }
 
     public void backButtonPressed(View view){
         this.onBackPressed();
     } //di app
 
-    public void clickSimpan(View view){
-        Intent next = new Intent(this, profile_pribadi.class);
-        startActivity(next);
+    private void getUserInfo() {
+        userDatabse.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0){
+                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                    if(map.get("name") != null){
+                        name = map.get("name").toString();
+                        fullName.setText(name);
+                    }
+                    if(map.get("address") != null){
+                        address = map.get("address").toString();
+                        alamat.setText(address);
+                    }
+                    if(map.get("dob") != null){
+                        dob = map.get("dob").toString();
+                        tanggalLahir.setText(dob);
+                    }
+                    if(map.get("occupation") != null){
+                        occupation = map.get("occupation").toString();
+                        pekerjaanPribadi.setText(occupation);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/taarufindonesia/profile_pribadi.java
+++ app/src/main/java/com/example/taarufindonesia/profile_pribadi.java
@@ -1,5 +1,6 @@
 package com.example.taarufindonesia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,21 +10,83 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
+
 public class profile_pribadi extends AppCompatActivity {
+    private TextView NamaLengkap, tanggalLahir_value, alamat_value, pekerjaanPribadi_value;
+    private Button Edit,logout;
+    private FirebaseAuth mAuth;
+    private DatabaseReference userDatabse;
+    private String userId, name, address, occupation, dob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_pribadi);
-        //-------------- kode passing data --------------
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_profile_pribadi);
 
+            NamaLengkap = (TextView) findViewById(R.id.Namalengkap);
+            tanggalLahir_value = (TextView) findViewById(R.id.tanggalLahir_value);
+            alamat_value = (TextView) findViewById(R.id.alamat_value);
+            pekerjaanPribadi_value = (TextView) findViewById(R.id.pekerjaanPribadi_value);
+            Edit = (Button) findViewById(R.id.Edit);
+
+            mAuth = FirebaseAuth.getInstance();
+            userId = mAuth.getCurrentUser().getUid();
+            userDatabse = FirebaseDatabase.getInstance().getReference().child("user").child(userId);
+
+            getUserInfo();
+        }
+
+    private void getUserInfo() {
+        userDatabse.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0){
+                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                    if(map.get("name") != null){
+                        name = map.get("name").toString();
+                        NamaLengkap.setText(name);
+                    }
+                    if(map.get("address") != null){
+                        address = map.get("address").toString();
+                        alamat_value.setText(address);
+                    }
+                    if(map.get("dob") != null){
+                        dob = map.get("dob").toString();
+                        tanggalLahir_value.setText(dob);
+                    }
+                    if(map.get("occupation") != null){
+                        occupation = map.get("occupation").toString();
+                        pekerjaanPribadi_value.setText(occupation);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     public void backButtonPressed(View view){
         this.onBackPressed();
     } //di app
+
     public void clickLogout(View view){
         Intent next = new Intent(this, HalamanAwal.class);
+        startActivity(next);
+    }
+
+    public void clickEdit(View view){
+        Intent next = new Intent(this, Settings.class);
         startActivity(next);
     }
 }
